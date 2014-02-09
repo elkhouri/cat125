0a15464a64ebbebe32d7000882648bff4cef1b8b
Alex Chen <elkhouri1@gmail.com>
2014-02-09 04:22:56 -0800

    home and about page with markdown

--- public/javascripts/directives.js
+++ public/javascripts/directives.js
@@ -1,11 +1,14 @@
 'use strict';
 
 /* Directives */
+angular.module('myApp.directives', []).directive('markdown', function() {
+    var converter = new Showdown.converter();
+    return {
+        restrict: 'E',
+        link: function(scope, element, attrs) {
+            var htmlText = converter.makeHtml(element.text());
+            element.html(htmlText);
+        }
+    }
+});
 
-
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
