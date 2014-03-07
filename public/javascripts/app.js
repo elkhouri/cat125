115be2c1a197da76e74fa3c5bb90bb4a8958e969
Alex Chen <elkhouri1@gmail.com>
2014-03-06 20:49:52 -0800

    Outline pages

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -10,5 +10,13 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/home', {templateUrl: 'templates/home.html', controller: 'MyCtrl1'});
   $routeProvider.when('/about', {templateUrl: 'templates/about.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/accessibility', {templateUrl: 'templates/accessibility.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/user', {templateUrl: 'templates/user.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/environment', {templateUrl: 'templates/environment.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/affordance', {templateUrl: 'templates/affordance.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/color-shape', {templateUrl: 'templates/color-shape.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/hierarchy', {templateUrl: 'templates/hierarchy.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/links', {templateUrl: 'templates/links.html', controller: 'MyCtrl2'});
+
   $routeProvider.otherwise({redirectTo: '/home'});
 }]);
